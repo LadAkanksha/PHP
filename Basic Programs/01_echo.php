44bc966ac3b2162edd0eea93b81171e1a26046c3
LadAkanksha <akankshalad2003@gmail.com>
2025-06-28 23:02:51 +0530

    Basic Concept

---
+++ Basic Programs/01_echo.php
@@ -0,0 +1,23 @@
+<?php
+    // How to print statement in php?
+    
+    //  1) Single Statement
+
+        echo "Hello World !!!";
+        echo 'Welcome in PHP !!!';
+        echo 199.143;
+
+    //  2) Multiple Statement
+
+        echo 'HTML','CSS','JavaScript', 'PHP ';
+        echo "C","C++","C#.NET  ";
+
+    //  3) Concate Statement / String
+
+         echo "My Name : "."Akanksha Chandrakant Lad ";
+         echo "Qualification : "."B.C.S ";
+         
+    //  3) Use of  HTML in PHP
+
+        echo "<br>I am Searching for a Job.</br>"
+?>
