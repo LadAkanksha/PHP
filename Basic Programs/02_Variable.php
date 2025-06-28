ef93c547c3e0afe4b040671f3ad3f18ecdae38b9
LadAkanksha <akankshalad2003@gmail.com>
2025-06-28 23:03:59 +0530

    Basic Concept

---
+++ Basic Programs/02_Variable.php
@@ -0,0 +1,18 @@
+<?php
+    //A variable is a named location in memory where a programmer can store a value
+
+    $var = "Welcome</br>";
+
+    echo $var;
+    echo $var;
+    echo $var;
+    echo $var;
+    echo $var;
+
+    $var = 2003;
+
+    echo $var;
+    echo "<br>";
+    $var = 0;
+    echo $var;
+?>
