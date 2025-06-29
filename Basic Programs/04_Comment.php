5f970f7c1a345473f747b581a879601cfc1a2f86
LadAkanksha <akankshalad2003@gmail.com>
2025-06-29 23:37:39 +0530

    Basic Concept

---
+++ Basic Programs/04_Comment.php
@@ -0,0 +1,31 @@
+<?php
+
+    /* Comments are used to increase readability of program
+                            OR
+        To disable the code from execution
+
+
+
+
+        TYPE -> 
+                1) Single Line Comment      -> i)   //
+                                              ii)   #
+
+                2) Multi-Line Comment       -> /* statements(s) */
+
+                
+        // Here is PHP Code
+        echo "Welcome in PHP World";
+
+        #echo 23+6;
+
+        /*"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad minus quos voluptate officia perspiciatis aliquam culpa. Debitis consequatur quam, mollitia officia, explicabo earum voluptatibus nulla temporibus commodi obcaecati iure perferendis?
+Eveniet distinctio numquam dignissimos magnam veritatis amet, culpa voluptate. Aperiam, eaque esse aspernatur sequi dicta ab culpa incidunt corporis ratione ut iste vero aliquam quae deserunt itaque reiciendis nesciunt reprehenderit.
+Reiciendis non perferendis vitae excepturi illum ab ad placeat ea officia dolorum quas iste tenetur necessitatibus, ipsum modi quis nobis, neque ullam. Autem inventore ipsa reiciendis illum necessitatibus harum minima.
+Repellat sequi corporis aliquam accusantium repellendus saepe officiis eligendi optio perferendis, dicta exercitationem error culpa amet sapiente repudiandae a minima ut quidem inventore. Natus dolorem aperiam enim corporis, esse totam.
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad minus quos voluptate officia perspiciatis aliquam culpa. Debitis consequatur quam, mollitia officia, explicabo earum voluptatibus nulla temporibus commodi obcecati iure perferendis?
+Eveniet distinctio numquam dignissimos magnam veritatis amet, culpa voluptate. Aperiam, eaque esse aspernatur sequi dicta ab culpa incidunt corporis ratione ut iste vero aliquam quae deserunt itaque reiciendis nesciunt reprehenderit.
+Reiciendis non perferendis vitae excepturi illum ab ad placeat ea officia dolorum";*/
+
+
+?>
