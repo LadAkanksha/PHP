235cf40718f72e752e1528889a1c6fa283416637
LadAkanksha <akankshalad2003@gmail.com>
2025-06-29 23:37:53 +0530

    Basic Concept

---
+++ Basic Programs/05_Constant_Variable.php
@@ -0,0 +1,15 @@
+<?php
+    define("cons","14.7");
+
+    echo cons;
+
+    define("Value","56.83",);
+
+    echo "<br>";
+    # define("cons","56");        
+    
+    # This Code Generates Error Bcz Constant values Assigned Once
+    # We can't reassign
+
+    echo Value;
+?>
