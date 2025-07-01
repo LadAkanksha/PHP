2ca65375e7c6f1cb0c2556508482596afd747a9a
LadAkanksha <akankshalad2003@gmail.com>
2025-07-01 23:06:20 +0530

    Conditional Statement

---
+++ Control Statement/Positive_Negative_Zero_Number.php
@@ -0,0 +1,11 @@
+<?php
+    $val = 129;
+
+    if($val == 0)
+      echo $val." is <u>Zero</u>";
+    elseif($val < 0)
+      echo $val." is <u>Negative</u>";
+    else
+      echo $val." is <u>Positive</u>";
+    
+?>
