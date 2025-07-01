1495c2118ff2422f79f1a8bb6af6274358793c50
LadAkanksha <akankshalad2003@gmail.com>
2025-07-01 23:05:25 +0530

    Conditional Statement

---
+++ Control Statement/A_Even_Odd_Number.php
@@ -0,0 +1,10 @@
+<?php
+    $value = 16;
+
+    if($value % 2 == 0):
+        echo $value." is Even Number.";
+    endif;
+    if($value % 2 != 0):
+        echo $value." is Odd Number.";
+    endif;
+?>
