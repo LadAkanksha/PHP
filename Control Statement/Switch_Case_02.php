8da59f5ae247a524cbe8e34a2c3e82d1863fc3e8
LadAkanksha <akankshalad2003@gmail.com>
2025-07-02 23:39:18 +0530

    Conditional Statement

---
+++ Control Statement/Switch_Case_02.php
@@ -0,0 +1,12 @@
+<?php
+    $day = 14;
+
+    switch ($day) 
+    {
+        case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+            echo "valid Day";
+            break;
+        default:
+            echo "Enter Valid Input!!!";
+    }
+?>
