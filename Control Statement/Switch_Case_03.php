cfe0eb477720d240afda3f134a9abe5b28570710
LadAkanksha <akankshalad2003@gmail.com>
2025-07-03 23:17:21 +0530

    Conditional Statement

---
+++ Control Statement/Switch_Case_03.php
@@ -0,0 +1,23 @@
+<?php
+    $day = 3;
+
+    switch ($day) 
+    {
+        case 1:
+            echo "Day is Monday<br>";
+        case 2:
+            echo "Day is Tuesday<br>";
+        case 3:
+            echo "Day is Wednsday<br>";
+        case 4:
+            echo "Day is Thursday<br>";
+        case 5:
+            echo "Day is Friday<br>";
+        case 6:
+            echo "Day is Saturday<br>";
+        case 7:
+            echo "Day is Sunday<br>";
+        default:
+            echo "Enter Valid Input!!!<br>";
+    }
+?>
