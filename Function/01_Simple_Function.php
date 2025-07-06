c8c49a581aeb9a677823bbed3d0496594cda9e87
LadAkanksha <akankshalad2003@gmail.com>
2025-07-06 23:12:14 +0530

    Function

---
+++ Function/01_Simple_Function.php
@@ -0,0 +1,14 @@
+<?php
+    function addition()
+    {
+        $no1 = 29 ;
+        $no2 = 31; 
+        $add = $no1 + $no2;
+
+        echo "Addition of ".$no1." + ".$no2." = ".$add."<br>";
+    }
+
+    addition();
+    addition();
+    addition();
+?>
