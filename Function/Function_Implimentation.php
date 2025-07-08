a3ba77307b4b05b4e1916d64698048a5424eb9cd
LadAkanksha <akankshalad2003@gmail.com>
2025-07-08 23:42:39 +0530

    Function

---
+++ Function/Function_Implimentation.php
@@ -0,0 +1,15 @@
+<?php
+
+    # Function - Function is block of code used to perform specific task.
+    #            Function reduce code and improve readability and modularity 
+    function show()     // Function Definition
+    {
+        echo "Hello World !!!";
+    }
+
+    show();                     // Function Call
+    show();                     // Function Call                     
+    show();                     // Function Call
+    show();                     // Function Call
+    show();                     // Function Call
+?>
